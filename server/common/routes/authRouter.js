3d2144e60c1e8ae3ccbac0cc4a4fed94197a6e41
Hermes_erm <srivathsan.ganesan@motherson.com>
2024-06-28 10:17:37 +0530

    from_init

--- server/common/routes/authRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const authRouter = express.Router();
-const { login, register } = require("../controllers/auth/authController");
-
-authRouter.post("/login", login);
-authRouter.post("/register", register);
-module.exports = authRouter;
