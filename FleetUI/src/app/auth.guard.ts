e4892a2243e4122a1c6398b00b26b8ad2c48c5e1
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-07-10 12:37:06 +0530

    Login Page Setup Commit

--- FleetUI/src/app/auth.guard.ts
+++ FleetUI/src/app/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from './auth.service';  // Adjust the path as needed
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +13,26 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const url: string = state.url;
+    return this.checkLogin(url);
+  }
+
+  checkLogin(url: string): boolean {
     if (this.authService.isLoggedIn()) {
+      if (url === '/') {
+        // User is logged in and trying to navigate to the login page, redirect them to project setup
+        this.router.navigate(['/project_setup']);
+        return false;
+      }
       return true;
-    } else {
+    }
+
+    if (url === '/project_setup') {
+      // User is not logged in and trying to access project setup, redirect them to login page
       this.router.navigate(['/']);
       return false;
     }
+
+    return true;
   }
 }
