b7df688939dcf57ce9f20d96b29d3f181281c7c0
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-07-10 11:24:21 +0530

    first commit

--- FleetUI/src/app/auth.guard.ts
+++ FleetUI/src/app/auth.guard.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
-import { AuthService } from './auth.service';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';  // Adjust the path as needed
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     if (this.authService.isLoggedIn()) {
       return true;
     } else {
