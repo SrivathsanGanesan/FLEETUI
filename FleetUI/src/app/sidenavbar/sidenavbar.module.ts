77383bef66539236ce6913a57f97cf4676e440c8
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-07-12 10:27:55 +0530

    Commit

---
+++ FleetUI/src/app/sidenavbar/sidenavbar.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidenavbarComponent } from './sidenavbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidenavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SidenavbarModule { }
