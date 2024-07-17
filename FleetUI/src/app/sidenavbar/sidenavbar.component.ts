4b800988d7dc4fee118ba6a4b5eba1cf55f8201f
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-07-17 15:55:24 +0530

    init_again

--- FleetUI/src/app/sidenavbar/sidenavbar.component.ts
+++ FleetUI/src/app/sidenavbar/sidenavbar.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-sidenavbar',
   templateUrl: './sidenavbar.component.html',
-  styleUrl: './sidenavbar.component.css'
+  styleUrl: './sidenavbar.component.css',
 })
 export class SidenavbarComponent {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private projectService: ProjectService
+  ) {}
 
+  logout() {
+    fetch('http://localhost:3000/auth/logout', {
+      credentials: 'include',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.isCookieDeleted);
+        if (data.isCookieDeleted) {
+          this.authService.logout();
+          this.router.navigate(['/']);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 }
--- server/fleetcore/models/sitesSchema.js
+++ server/fleetcore/models/sitesSchema.js
@@ -1,6 +1,7 @@
 const { Schema, model, mongoose } = require("mongoose");
 const { projectConnection } = require("../../common/db_config");
 
+// dup uh..
 const mapSchema = new Schema(
   {
     mapName: {
@@ -19,6 +20,7 @@ const mapSchema = new Schema(
   { versionKey: false }
 );
 
+// dup uh..
 const mapModel = projectConnection.model("map", mapSchema, "map");
 
 const projMapSchema = new Schema(
