4d1193315ba2e0bbd76ff28df576509d7bdd2d18
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-07-30 15:21:20 +0530

    ch

--- FleetUI/src/app/uptime/uptime.component.ts
+++ FleetUI/src/app/uptime/uptime.component.ts
@@ -1,11 +1,12 @@
 import { Component, ViewChild } from "@angular/core";
+
 import {
   ApexNonAxisChartSeries,
   ApexPlotOptions,
   ApexChart,
   ApexFill,
-  ApexAnnotations,
-  ChartComponent
+  ChartComponent,
+  ApexStroke
 } from "ng-apexcharts";
 
 export type ChartOptions = {
@@ -14,7 +15,7 @@ export type ChartOptions = {
   labels: string[];
   plotOptions: ApexPlotOptions;
   fill: ApexFill;
-  annotations: ApexAnnotations;
+  stroke: ApexStroke;
 };
 
 @Component({
@@ -24,113 +25,47 @@ export type ChartOptions = {
 })
 export class UptimeComponent {
   @ViewChild("chart") chart!: ChartComponent;
-  public chartOptions: Partial<ChartOptions> & {
-    series: ApexNonAxisChartSeries;
-    chart: ApexChart;
-    labels: string[];
-    plotOptions: ApexPlotOptions;
-    fill: ApexFill;
-    annotations: ApexAnnotations;
-  };
+  public chartOptions: Partial<ChartOptions>;
 
   constructor() {
     this.chartOptions = {
-      series: [74],
+      series: [67],
       chart: {
+        height: 400,
         type: "radialBar",
-        height: 300, // Chart height
-        width: 300, // Chart width
-        offsetY: 0
+        offsetY: -10
       },
       plotOptions: {
         radialBar: {
-          startAngle: -100,
-          endAngle: 100,
-          track: {
-            background: "#7E7777",
-            strokeWidth: "100%",
-            margin: 0,
-            dropShadow: {
-              enabled: true,
-              top: 2,
-              left: 0,
-              opacity: 0.31,
-              blur: 5
-            }
-          },
+          startAngle: -125,
+          endAngle: 125,
           dataLabels: {
             name: {
-              show: true, // Show the name
-              offsetY: -10,
-              fontSize: "15px" // Adjust font size
+              fontSize: "16px",
+              color: '#ff7373',
+              offsetY: 120,
             },
             value: {
-              offsetY: 20, // Adjust position to below the chart
+              offsetY: 76,
               fontSize: "30px",
-              color: "#ff7373", // Color of the value text
+              color: undefined,
               formatter: function(val) {
-                return val + "%"; // Add percentage symbol
+                return val + "%";
               }
             }
           }
         }
       },
       fill: {
-        type: "gradient",
-        gradient: {
-          shade: "light",
-          shadeIntensity: 0.4,
-          inverseColors: false,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 50, 53, 91],
-          colorStops: [
-            {
-              offset: 0,
-              color: "#ff7373",
-              opacity: 1
-            },
-            {
-              offset: 100,
-              color: "#ff7373",
-              opacity: 1
-            }
-          ]
-        }
+        type: "solid",
+        colors: ['#ff7373'] // Solid color
       },
-      labels: [""],
-      annotations: {
-        yaxis: [
-          {
-            y: 0,
-            borderColor: "#ff7373",
-            label: {
-              borderColor: "#ff7373",
-              style: {
-                color: "#fff",
-                background: "#ff7373",
-                fontSize: "12px" // Reduced font size
-              },
-              text: "0%",
-              offsetX: -10 // Move the label to the left
-            }
-          },
-          {
-            y: 100,
-            borderColor: "#ff7373",
-            label: {
-              borderColor: "#ff7373",
-              style: {
-                color: "#fff",
-                background: "#ff7373",
-                fontSize: "12px" // Reduced font size
-              },
-              text: "100%",
-              offsetX: 10 // Move the label to the right
-            }
-          }
-        ]
-      }
+      stroke: {
+        dashArray: 3, // Remove dash for solid stroke
+        colors: ['#ff7373'], // Stroke color
+        width: -30 // Increase stroke width
+      },
+      labels: ["Percent"]
     };
   }
 }
