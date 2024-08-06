25bbe65c3b768eba947915d3fc8cb27a78ad8230
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-08-06 10:13:37 +0530

    Revert "init_lst"
    This reverts commit c80c4cdf01aba7c40a7f2b40ccc1640ae319e66f.

--- server/fleetcore/controllers/fileController/fileImportController.js
+++ server/fleetcore/controllers/fileController/fileImportController.js
@@ -188,13 +188,11 @@ const parseProjectFile = async (req, res, next) => {
   // let alterName = "altered_name";
   const target = path.resolve("./proj_assets/projectFile/");
   let responseSent = false;
-  // console.log(responseSent);
 
   const sendResponse = (status, body) => {
     if (!responseSent) {
-      responseSent = true;
       res.status(status).json(body);
-      // res.end();
+      responseSent = true;
     }
   };
 
