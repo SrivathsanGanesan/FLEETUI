124a9731465111e059dc4868013ef27ace018d30
Vigneswara-robis <vigneswara.gopalsamy@motherson.com>
2024-08-13 12:56:24 +0530

    Update server_config.js

--- server/common/server_config.js
+++ server/common/server_config.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:4200",
+      "http://localhost:4201",
       // "http://192.168.11.183:3000",
       // "http://localhost:57065",
     ], // WIP...
