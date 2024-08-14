a6a2ae8b6409a2a7a73d23efd1f6c892aa9aec0e
Srivathsanganesan <srivathsan.ganesan@motherson.com>
2024-08-14 15:46:53 +0530

    init_lst

--- server/fleetcore/controllers/configuration/fleetConfiguration.js
+++ server/fleetcore/controllers/configuration/fleetConfiguration.js
@@ -1,14 +1,28 @@
 const { getIPRange } = require("get-ip-range");
 const NetworkScanner = require("network-scanner-js");
-// const nmap = require("libnmap");
 const netScan = new NetworkScanner();
 const arp = require("node-arp");
-const ping = require("ping"); //..
-const ipRangeCheck = require("ip-range-check"); //..
+
+const eventStreamHeader = {
+  "Content-Type": "text/event-stream",
+  "Cache-Control": "no-cache",
+  Connection: "keep-alive",
+};
 
 const scanIp = async (req, res, next) => {
   let arr = [];
-  const ipv4Range = getIPRange("52.95.110.0-52.95.110.200");
+  const ipv4Range = getIPRange("192.168.249.3-192.168.249.248");
+  const macPromises = ipv4Range.map((ip) => {
+    return new Promise((resolve, reject) => {
+      arp.getMAC(ip, (err, mac) => {
+        if (err) {
+          reject(err); // Reject promise on error
+        } else {
+          resolve(mac); // Resolve promise with MAC address
+        }
+      });
+    });
+  });
 
   const ipRange = [
     "192.168.249.3",
@@ -16,6 +30,7 @@ const scanIp = async (req, res, next) => {
     "192.168.249.248",
     "142.250.182.46",
   ]; // Add your IP range
+  res.writeHead(200, eventStreamHeader);
   for (let ip of ipRange) {
     const poll = await netScan.poll(ip, {
       //192.168.249.183
@@ -23,21 +38,23 @@ const scanIp = async (req, res, next) => {
       size: 32,
       timeout: null,
     });
-    arp.getMAC(ip, (err, mac) => {
-      if (!err) {
-        arr.push(mac);
-        console.log(mac);
-      }
+    const netPoll = JSON.stringify({
+      host: poll.host,
+      ip_address: poll.ip_address,
+      status: poll.status,
+      time: poll.res_avg,
     });
-    // arr.push({
-    //   host: poll.host,
-    //   ip_address: poll.ip_address,
-    //   status: poll.status,
-    //   time: poll.res_avg,
-    // });
+    res.write(`data: ${netPoll}\n\n`);
   }
+  const macAddresses = await Promise.all(macPromises);
+  console.log(macAddresses);
+  res.end();
 
-  res.json(arr);
+  res.on("close", () => {
+    return res.end();
+  });
+
+  // res.json(arr);
 };
 
 module.exports = { scanIp };
