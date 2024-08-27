e2686177df625339212885db64b247b217e19804
SrivathsanGanesan <srivathsan.ganesan@motherson.com>
2024-08-27 16:28:14 +0530

    c

--- FleetUI/src/app/tasks/tasks.component.ts
+++ FleetUI/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
--- FleetUI/src/app/tasks/tasks.module.ts
+++ FleetUI/src/app/tasks/tasks.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from '../tasks/tasks.component';
+import { SidenavbarComponent } from '../sidenavbar/sidenavbar.component';
 
 @NgModule({
-  declarations: [TasksComponent],
+  declarations: [],
   imports: [CommonModule],
 })
 export class TasksModule {}
