f8b7f85835474e1e332a92040f611aad062b6e8c
Sudhagaran <Sudhagaran.Rajendran@motherson.com>
2024-08-28 15:52:33 +0530

    initlst

--- FleetUI/src/app/configuration/configuration.component.ts
+++ FleetUI/src/app/configuration/configuration.component.ts
@@ -13,8 +13,6 @@ import { environment } from '../../environments/environment.development';
 import { ProjectService } from '../services/project.service';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 
-
-
 interface Poll {
   ip: string;
   mac: string;
