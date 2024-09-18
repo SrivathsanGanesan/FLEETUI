0fc246d80b09bb48b43aa0606159645ee0aa2dc4
Sudhagaran <Sudhagaran.Rajendran@motherson.com>
2024-09-18 15:50:06 +0530

    changes with robo logs

--- server/fleetcore/routes/liveStreamRouter.js
+++ server/fleetcore/routes/liveStreamRouter.js
@@ -3,11 +3,13 @@ const {
   getAgvTelemetry,
   getGrossTaskStatus,
   getRoboStateCount,
+  getRoboActivities,
 } = require("../controllers/liveStreamController/telemetryController");
 
 const liveStreamRouter = express.Router();
 liveStreamRouter.get("/live-AMR-pos/:mapId", getAgvTelemetry);
 liveStreamRouter.post("/get-tasks-status/:mapId", getGrossTaskStatus);
 liveStreamRouter.post("/get-robos-state/:mapId", getRoboStateCount);
+liveStreamRouter.post("/get-robo-activities", getRoboActivities);
 
 module.exports = liveStreamRouter;
