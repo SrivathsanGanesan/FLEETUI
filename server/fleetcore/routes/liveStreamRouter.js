e123a3e7968a4f69a2ab7bf6b613f918c86eccca
Sudhagaran <Sudhagaran.Rajendran@motherson.com>
2024-10-18 15:15:48 +0530

    changes with robot updates with integ

--- server/fleetcore/routes/liveStreamRouter.js
+++ server/fleetcore/routes/liveStreamRouter.js
@@ -7,16 +7,18 @@ const {
   getRoboPos,
   showSpline,
   enableRobo,
+  getLiveRobos,
 } = require("../controllers/liveStreamController/telemetryController");
 
 const liveStreamRouter = express.Router();
 // liveStreamRouter.get("/live-AMR-pos/:mapId", getAgvTelemetry); // yet to gothrough..
+liveStreamRouter.post("/get-fms-amrs", getRoboDetails);
+liveStreamRouter.post("/initialize-robot", initializeRobo);
+liveStreamRouter.post("/enable-robot", enableRobo);
+liveStreamRouter.get("/get-live-robos/:mapId", getLiveRobos);
 liveStreamRouter.get("/live-AMR-pos/:mapId", getRoboPos);
 liveStreamRouter.post("/get-robos-state/:mapId", getRoboStateCount);
 liveStreamRouter.post("/get-robo-activities", getRoboActivities);
-liveStreamRouter.post("/initialize-robot", initializeRobo);
-liveStreamRouter.post("/get-fms-amrs", getRoboDetails);
 liveStreamRouter.post("/show-spline", showSpline);
-liveStreamRouter.post("/enable-robot", enableRobo);
 
 module.exports = liveStreamRouter;
