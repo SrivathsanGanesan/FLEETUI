508713b09bb7ef267e47998b2cdc8d2011947c40
Sudhagaran <Sudhagaran.Rajendran@motherson.com>
2024-10-21 14:49:59 +0530

    changes with nodata pic

--- FleetUI/src/app/support/support.component.ts
+++ FleetUI/src/app/support/support.component.ts
@@ -1,66 +1,102 @@
 import { Component } from '@angular/core';
-
 import { Router } from '@angular/router'; // Import Router
+import { environment } from '../../environments/environment.development';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
-  styleUrl: './support.component.css'
+  styleUrl: './support.component.css',
 })
 export class SupportComponent {
-
   showPopup = false;
-  
 
   // Sample questions and answers
   questions = [
-    { question: 'Is there a free trial available?', answer: 'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free 30-minute onboarding call to get you up and running.', showAnswer: false },
-    { question: 'What is your cancellation policy?', answer: 'We understand that things change. You can cancel your plan at any time and we’ll refund you the difference already paid.', showAnswer: false },
-    { question: 'How does billing work?', answer: 'Plans are per workspace, not per account. You can upgrade one workspace, and still have any number of free workspaces.', showAnswer: false },
-    { question: 'How does support work?', answer: 'If you’re having trouble, we’re here to help via email. We’re a small team but will get back to you soon.', showAnswer: false },
-    { question: 'Can I change my plan later?', answer: 'Of course you can! Our pricing scales with your company. Chat with our friendly team to find a solution that works for you as you grow.', showAnswer: false },
-    { question: 'Can other info be added to an invoice?', answer: 'At the moment, the only way to add additional information to invoices is to add it to the workspace’s name manually.', showAnswer: false },
-    { question: 'How do I change my account email?', answer: 'You can change the email address associated with your account by going to your account settings on a laptop or desktop.', showAnswer: false },
-    { question: 'Do you provide tutorials?', answer: 'Not yet, but we’re working on it! We’ve done our best to make it intuitive and are building out the documentation.', showAnswer: false }
-];
-
+    {
+      question: 'Is there a free trial available?',
+      answer:
+        'Yes, you can try us for free for 30 days. If you want, we’ll provide you with a free 30-minute onboarding call to get you up and running.',
+      showAnswer: false,
+    },
+    {
+      question: 'What is your cancellation policy?',
+      answer:
+        'We understand that things change. You can cancel your plan at any time and we’ll refund you the difference already paid.',
+      showAnswer: false,
+    },
+    {
+      question: 'How does billing work?',
+      answer:
+        'Plans are per workspace, not per account. You can upgrade one workspace, and still have any number of free workspaces.',
+      showAnswer: false,
+    },
+    {
+      question: 'How does support work?',
+      answer:
+        'If you’re having trouble, we’re here to help via email. We’re a small team but will get back to you soon.',
+      showAnswer: false,
+    },
+    {
+      question: 'Can I change my plan later?',
+      answer:
+        'Of course you can! Our pricing scales with your company. Chat with our friendly team to find a solution that works for you as you grow.',
+      showAnswer: false,
+    },
+    {
+      question: 'Can other info be added to an invoice?',
+      answer:
+        'At the moment, the only way to add additional information to invoices is to add it to the workspace’s name manually.',
+      showAnswer: false,
+    },
+    {
+      question: 'How do I change my account email?',
+      answer:
+        'You can change the email address associated with your account by going to your account settings on a laptop or desktop.',
+      showAnswer: false,
+    },
+    {
+      question: 'Do you provide tutorials?',
+      answer:
+        'Not yet, but we’re working on it! We’ve done our best to make it intuitive and are building out the documentation.',
+      showAnswer: false,
+    },
+  ];
 
   leftQuestions = this.questions.slice(0, 4);
   rightQuestions = this.questions.slice(4);
 
   toggleAnswer(selectedQuestion: any) {
     // Close all questions first
-    this.leftQuestions.forEach(question => {
+    this.leftQuestions.forEach((question) => {
       if (question !== selectedQuestion) {
         question.showAnswer = false;
       }
     });
-    this.rightQuestions.forEach(question => {
+    this.rightQuestions.forEach((question) => {
       if (question !== selectedQuestion) {
         question.showAnswer = false;
       }
     });
-  
+
     // Toggle the selected question
     selectedQuestion.showAnswer = !selectedQuestion.showAnswer;
   }
-  
 
   goToFaq() {
-      this.showPopup = true; // Show the popup
+    this.showPopup = true; // Show the popup
   }
 
   closePopup() {
-      this.showPopup = false; // Hide the popup
+    this.showPopup = false; // Hide the popup
   }
 
-
+  exportProject() {}
 
   goToDocumentation(): void {
     window.open('/documentation', '_blank');
   }
 
- 
   // Document for resource
   downloadDocument() {
     const link = document.createElement('a');
@@ -70,6 +106,4 @@ export class SupportComponent {
     link.click();
     document.body.removeChild(link);
   }
-
-
 }
